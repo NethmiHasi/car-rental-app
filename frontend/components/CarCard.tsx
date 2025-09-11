7dd35c2668bb627ccc39bc46628b4cfc63213dca
NethmiHasi <nethuhasi2001@gmail.com>
2025-09-11 13:35:46 +0700

    add car card component

---
+++ frontend/components/CarCard.tsx
@@ -0,0 +1,76 @@
+"use client";
+import Image from "next/image";
+
+interface Car {
+  id: string;
+  make: string;
+  model: string;
+  price: number;
+  year: number
+  image: string;
+  seat: number;
+  availability: boolean;
+  description: string;
+}
+
+interface CarCardProps {
+  car: Car;
+  onBooking?: (id: string) => void;
+  onClick?: () => void
+  detailed?: boolean;
+}
+
+const CarCard = ({ car, onBooking, onClick, detailed = false }: CarCardProps) => {
+
+  return (
+    <div
+      className={`bg-white rounded-3xl shadow-2xl overflow-hidden transition-all ${detailed
+        ? "w-full max-w-4xl mx-auto flex flex-col lg:flex-row gap-10 p-8"
+        : "hover:shadow-3xl transform hover:scale-105 w-64"
+        }`}
+      onClick={onClick}
+    >
+      <div className={`relative ${detailed ? "w-full lg:w-1/2 h-80 lg:h-[400px]" : "w-full h-44"}`}>
+        <Image
+          src={car.image}
+          alt={car.make}
+          fill
+          className={`object-cover rounded-3xl ${detailed ? "shadow-2xl" : ""}`}
+        />
+      </div>
+      <div className={`p-4 ${detailed ? "lg:w-1/2 space-y-4 text-left" : "text-center"}`}>
+        <h3 className={`text-indigo-700 font-bold ${detailed ? "text-4xl" : "text-xl"}`}>
+          {car.make} {car.model}
+        </h3>
+        {detailed && (
+          <>
+            <p className="text-gray-600">{car.description}</p>
+            <p className="text-lg">Year: {car.year}</p>
+            <p className="text-lg">Seats: {car.seat}</p>
+            <p
+              className={car.availability ? "text-green-600 font-semibold" : "text-red-600 font-semibold"}
+            >
+              {car.availability ? "Available" : "Not Available"}
+            </p>
+          </>
+        )}
+        <p className={`text-indigo-600 font-bold ${detailed ? "text-2xl" : "text-lg"}`}>${car.price}/day</p>
+        {onBooking && (
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              onBooking(car.id);
+            }}
+            className="w-full bg-indigo-600 text-white py-2 rounded-full hover:bg-indigo-700 transition mt-2"
+          >
+            Book Now
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+
+
+export default CarCard
--- frontend/components/index.ts
+++ frontend/components/index.ts
@@ -2,10 +2,12 @@ import CarCard from "./CarCard";
 import AuthListener from "./AuthListener";
 import Providers from "./Provider";
 import Navbar from "./Navbar";
+import HeroSection from "./HeroSection";
 
 export {
     CarCard,
     AuthListener,
     Providers,
     Navbar,
+    HeroSection,
 }
