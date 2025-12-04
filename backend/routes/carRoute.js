5328f8ff973db6280e102ac3cb3d4a156b746fb4
NethmiHasi <nethuhasi2001@gmail.com>
2025-12-04 20:40:35 +0530

    update car route

--- backend/routes/carRoute.js
+++ backend/routes/carRoute.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { listCar, removeCar } from "../controllers/carController";
+import { listCar, removeCar, addCar } from "../controllers/carController";
 import multer from "multer";
 
 const carRouter = express.Router();
 
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
+
+const upload = multer({storage:storage});
+
+carRouter.get("/add", upload.single("image"), addCar);
 carRouter.get("/list", listCar);
 carRouter.post("/remove", removeCar);
 
